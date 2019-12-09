1894ee9a6fd2f92856453fcb0204ec4e0cbaa1ea
qiu <qiuzhijiee@gmail.com>
2019-12-09 09:42:03 +0800

    我还是觉得不错

--- Hello.java
+++ Hello.java
@@ -5,5 +5,6 @@ public class Hello(){
         System.out.println("Hello, world!");
         System.out.println("Hello, world!");
         System.out.println("Hello, ZF!");
+        System.out.println("Hello, myself!");
     }
 }
