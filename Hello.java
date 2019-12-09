f4ee280b8b3ac9ff1e24a3c3f53f21f5d95086f6
qiu <qiuzhijiee@gmail.com>
2019-12-09 09:26:05 +0800

    1

--- Hello.java
+++ Hello.java
@@ -3,5 +3,6 @@ package com.likeStudy.bean
 public class Hello(){
     public  static void main(String[] args){
         System.out.println("Hello, world!");
+        System.out.println("Hello, world!");
     }
 }
