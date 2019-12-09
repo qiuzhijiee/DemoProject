321584c7ed8fa38e471c4e3cfbf1b989f666baca
qiu <qiuzhijiee@gmail.com>
2019-12-09 11:00:14 +0800

    ChangeToo

---
+++ Hello.java
@@ -0,0 +1,13 @@
+package com.likeStudy.bean
+
+public class Hello(){
+    public  static void main(String[] args){
+        System.out.println("Hello, world!");
+        System.out.println("Hello, world!");
+        System.out.println("Hello, ZF!");
+        System.out.println("Hello, ZF!");
+        System.out.println("Hello, myself!");
+        System.out.println("Hello, myself!");
+        System.out.println("Hello, myself!");
+    }
+}
